e475bf4b946937f63d411c92454ce30b32ee3f53
Hayun <mycolki@gmail.com>
2021-09-06 23:08:21 +0900

    feat: post/channel controller

---
+++ constants/errors/validationMessage.js
@@ -0,0 +1,4 @@
+exports.VALIDATION_MSG = {
+  ALREADY_EXIST: '같은 이름의 채널이 존재합니다. 다른 이름으로 입력해주세요',
+  FILL_BLANK: '빈칸 없이 모두 입력하고 채널개설을 눌러주세요',
+};
--- routes/channel.js
+++ routes/channel.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
+const validateBody = require('./middleware/validateBody');
 const channelController = require('./controllers/channel.controller');
 
 router.get('/', channelController.getChannels);
 router.get('/:channelId', channelController.getChannel);
+
+router.post('/', validateBody, channelController.createChannel);
 
 module.exports = router;
