0e59bceb0c51f74a88459d078a5d25862563fc7f
Hayun <mycolki@gmail.com>
2021-09-06 13:59:47 +0900

    feat: add episode & character schema

---
+++ models/Episode.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const episodeSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		isRequired: true,
+	},
+	videoUrl: {
+		type: String,
+		isRequired: true,
+	},
+	characters: [
+		{
+			type: ObjectId,
+			ref: 'Character',
+		}
+	],
+	scripts: [
+		{
+			time: {
+				type: Number,
+				isRequired: true,
+			},
+			character: {
+				type: ObjectId,
+				ref: 'Character',
+			},
+		}
+	],
+});
+
+module.exports = mongoose.model('Episode', episodeSchema);
