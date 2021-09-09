fe0d9b2bdf004c5b0938ef2c69d337d93f82580a
Hayun <mycolki@gmail.com>
2021-09-09 14:05:36 +0900

    feat: get episode data by episodeid

--- routes/controllers/episode.controller.js
+++ routes/controllers/episode.controller.js
@@ -4,13 +4,34 @@ const mongoose = require('mongoose');
 const Episode = require('../../models/Episode');
 const { ERR_MSG } = require('../../constants/errors/errorMessage');
 
-exports.getEpisodes = async function (_, res, next) {
+exports.getEpisodes = async (_, res, next) => {
   try {
     const episodes = await Episode.find().exec();
 
     res.json({
       result: 'ok',
       data: episodes,
+    });
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      return res.status(400).json({
+        result: 'error',
+        message: ERR_MSG.INVALID_DATA,
+      });
+    }
+
+    next(createError(500, ERR_MSG.SERVER_ERR));
+  }
+};
+
+exports.getSelectedEpisode = async (req, res, next) => {
+  try {
+    const { episodeId } = req.params;
+    const episode = await Episode.findById(episodeId);
+
+    res.json({
+      result: 'ok',
+      data: episode,
     });
   } catch (err) {
     if (err instanceof mongoose.Error.ValidationError) {
