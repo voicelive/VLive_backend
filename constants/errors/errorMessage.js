a522469ce6e232c9e302f81a823f6f16edb78501
eunbin <eunbines2@gmail.com>
2021-09-11 06:27:05 +0900

    chores: convert constant variables to korean

--- constants/errors/errorMessage.js
+++ constants/errors/errorMessage.js
@@ -1,6 +1,8 @@
 exports.ERR_MSG = {
-  SERVER_ERR: 'Server Error',
-  NOT_FOUND: 'Page Not Found',
-  INVALID_DATA: 'Invalid data',
-  BAD_REQUEST: 'Bad Request',
+  SERVER_ERR: '예상치 못한 에러가 발생했습니다.',
+  NOT_FOUND: '페이지를 찾을 수 없습니다.',
+  INVALID_DATA: '유효하지 않은 데이터입니다.',
+  BAD_REQUEST: '잘못된 요청입니다.',
+  TOKEN_EXPIRED: '유효하지 않은 토큰입니다.',
+  UNAUTHENTICATED: '인증되지 않은 사용자입니다.',
 };
