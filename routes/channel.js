854d818aff41bd460ca69888c8a27887a9ba51c8
Hayun <mycolki@gmail.com>
2021-09-11 17:59:00 +0900

    feat: add validateObjectId middleware

--- routes/channel.js
+++ routes/channel.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const validateObjectId = require('./middleware/validateObjectId');
 const validateBody = require('./middleware/validateBody');
 const channelController = require('./controllers/channel.controller');
 
 router.get('/', channelController.getChannels);
-router.get('/:channelId', channelController.getChannel);
-router.get('/:channelId/users/:userId', channelController.getUserType);
-router.put('/:channelId', channelController.updateChannel);
+router.get('/:channelId', validateObjectId, channelController.getChannel);
+router.get(
+  '/:channelId/users/:userId',
+  validateObjectId,
+  channelController.getUserType,
+);
 
+router.put('/:channelId', validateObjectId, channelController.updateChannel);
 router.post('/', validateBody, channelController.createChannel);
 
 module.exports = router;
--- routes/episode.js
+++ routes/episode.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const validateObjectId = require('./middleware/validateObjectId');
 const episodeController = require('./controllers/episode.controller');
 
 router.get('/', episodeController.getEpisodes);
-router.get('/:episodeId', episodeController.getEpisode);
+router.get('/:episodeId', validateObjectId, episodeController.getEpisode);
 
 module.exports = router;
